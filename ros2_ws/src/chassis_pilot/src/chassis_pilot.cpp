ab078a726e44b0ca805ad524cbcef5efdf609079
lukec <luke.yiru.chou@gmail.com>
2026-09-10 22:33:13 +0800

    s ss

--- ros2_ws/src/chassis_pilot/src/chassis_pilot.cpp
+++ ros2_ws/src/chassis_pilot/src/chassis_pilot.cpp
@@ -245,8 +245,8 @@ void ChassisPilot::control_loop() {
     current_goal_handle_->publish_feedback(feedback);
 
     RCLCPP_INFO_THROTTLE(this->get_logger(), *this->get_clock(), 500,
-        "追隨點: [%zu/%zu] | 剩餘總距: %.2f米 | 剩餘角度: %.2fRad | 目前線速: %.2f", 
-        current_waypoint_idx_ + 1, trajectory_.size(), remaining_dist, yaw_to_goal_, v_final);
+        "追隨點: [%zu/%zu] | 座標: %.2f,%.2f | 剩餘角度: %.2fRad | 目前線速: %.2f", 
+        current_waypoint_idx_ + 1, trajectory_.size(), x_, y_, yaw_to_goal_, v_final);
 }
 
 void ChassisPilot::stop_robot() {
